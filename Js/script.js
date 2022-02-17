311e2580ac59bba223fcdc4ea64da5d59b9eaef8
Amomen <amomen424@gmail.com>
2022-02-17 22:17:58 +0600

    remaining balance completed

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,4 @@
-document.getElementById('calculate').addEventListener('click', function () {
-    
+function calculation(positive) {
     //food
     const food = document.getElementById('food');
     const foodInt = parseFloat(food.value);
@@ -14,8 +13,63 @@ document.getElementById('calculate').addEventListener('click', function () {
     const costing = document.getElementById('costing');
     const costingInt = parseFloat(costing.innerText);
     const a = foodInt + rentInt + clothsInt;
-    costing.innerText=a;
+    costing.innerText = a;
+
+    // exclude costing from income
+    const income = document.getElementById('income');
+    const incomeInt = parseFloat(income.value);
+    const afterCosting = incomeInt - a;
+
+    //Show total balance after costing
+    const balance = document.getElementById('balance');
+    const balanceInt = parseFloat(balance.innerText);
+    balance.innerText = afterCosting;
+
+
+
+}
+
+
+
+
+
+
+document.getElementById('calculate').addEventListener('click', function () {
+
+    calculation()
+
+
+})
+
+//savings
+document.getElementById('sBtn').addEventListener('click', function () {
+
+    //catch the income
+    const income = document.getElementById('income');
+    const incomeInt = parseFloat(income.value);
+
+    //saving Input
+    const saving = document.getElementById('save');
+    const sFloat = parseFloat(saving.value);
+
+    //saving calculation
+    const parcent =parseFloat(incomeInt * sFloat) / 100;
+
+    // console.log(parcent)
+
+    //saving Show
+     document.getElementById('sAmount').innerText= parcent;
     
+
+    //remaining balance
+    const balance = document.getElementById('balance');
+    const balanceInt = parseFloat(balance.innerText);
+    document.getElementById('rBalance').innerText= balanceInt - parcent;
+
+
+
+    
+
 
 
 })
